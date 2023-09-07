dc6a1e3a8865f1a7a1eea696a819a07e12531b63
zsluffy <yonilaga2423@gmail.com>
2023-09-07 12:58:02 -0700

    unknown

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include <unistd.h>
-
 /**
- * main - main function 
+ * main - Entry point
  *
- * Return: 1 for success
+ * Return: Always o (success)
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
