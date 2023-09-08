15b72c9e75941a77f271e9aadb27d92f599c8903
zsluffy <yonilaga2423@gmail.com>
2023-09-08 08:47:00 -0700

    unknown

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = 0; i < 10; i++)
-		putchar(i);
-	putchar('\n');
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
 
 	return (0);
 }
