2ccedc7e5dc5f9a6007c9ccdec2f1742f80a40e4
zsluffy <yonilaga2423@gmail.com>
2023-09-18 07:35:21 -0700

    unknown

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - 
+ * _puts - prints a string
+ * @str: string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
