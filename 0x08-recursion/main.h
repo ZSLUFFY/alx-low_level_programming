22abdd9c59adb1aad9bf84c47ac7140c32ebd032
zsluffy <yonilaga2423@gmail.com>
2023-09-29 06:46:07 -0700

    unknwn

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,5 +13,6 @@ int actual_sqrt_recursion(int n, int i);
 int actual_prime(int n, int i);
 int is_palindrome(char *s);
 int check_pal(char *s, int i, int len);
+int wildcmp(char *s1, char *s2);
 
 #endif
