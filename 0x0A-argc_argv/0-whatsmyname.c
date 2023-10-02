9daba877b1e4474a87e4c0befcebb067805edf7d
zsluffy <yonilaga2423@gmail.com>
2023-10-02 08:41:26 -0700

    unknown

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name
+ * @argc: nume argument
+ * @argv: array arrg
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
