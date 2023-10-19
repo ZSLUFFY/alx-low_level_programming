02844b2c332ecf5f1ce0c1c2003872b70837d8d4
zsluffy <yonilaga2423@gmail.com>
2023-10-19 09:21:37 -0700

    unknown

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all list
+ * @h: linked list
+ *
+ * Return: The no of nudes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
