a424da5c875fd87dec815057fe1d6fc6195059c0
zsluffy <yonilaga2423@gmail.com>
2023-10-24 05:02:10 -0700

    unknown

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
+		nodes++;
 		printf("%d", h->n);
-		num++;
 		h = h->next;
 	}
-	return (num);
+	return (nodes);
 }
