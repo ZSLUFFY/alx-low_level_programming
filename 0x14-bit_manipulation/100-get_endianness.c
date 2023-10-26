1014ac3343c92fc1c959f3edbdedf90265fc875e
zsluffy <yonilaga2423@gmail.com>
2023-10-26 09:47:52 -0700

    unknown

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0
+ */
+
+int get_endianness(void)
+{
+	int a;
+	char *b;
+
+	a = 1;
+	b = (char*) & a;
+
+	return (*b);
+}
