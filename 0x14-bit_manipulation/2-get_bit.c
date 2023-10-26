69d81eb024c6c8f5420321f7931abeb8b9fc1cd1
zsluffy <yonilaga2423@gmail.com>
2023-10-26 04:46:44 -0700

    unknown

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
 /**
- * get_bit -
- * @n:
- * @index:
+ * get_bit - returns value
+ * @n: no to check
+ * @index: check bit
  *
- * Return:
+ * Return: value
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	
+	unsigned long int divisor, check;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	divisor = 1 << index;
+	check = n & divisor;
+	if (check == divisor)
+		return (1);
+
+	return (0);
 }
